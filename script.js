8b0d21a27434d0a922ef473018b48f9ee0410b9c
Madson <67187216+itsmadson@users.noreply.github.com>
2023-11-30 20:21:38 +0330

    Update script.js

--- script.js
+++ script.js
@@ -9,23 +9,23 @@ const lessonData = {
 // Define a mapping of lessons to their links
 const lessonLinks = {
   'english': {
-    'Lec1': 'https://drive.google.com/file/d/1Frqkx_GmV8H4Ov5aBZAIubsY-Yz96UV1/view?usp=drive_link',
-    'Lec2': 'https://drive.google.com/file/d/1c8qlOIfV6wVX8id1aYPYGT19AsGMSXY9/view?usp=sharing',
-    'Lec3': 'https://drive.google.com/file/d/1aGWWVvvcS0E2wHaG2ZFlLWOvZJ05bJzq/view?usp=drive_link'
+    'Lec1': 'Ur link',
+    'Lec2': 'Ur link',
+    'Lec3': 'Ur link'
   },
   'math': {
-    'Lec1': 'https://drive.google.com/file/d/1QVp54-LSpgneU_Dr4nFoTKsxP3RhPsbJ/view?usp=drive_link',
-    'Lec2': 'https://drive.google.com/file/d/1NzMKdIFeL1qb7ALWdX2zCNktcmjTQ-Ej/view?usp=drive_link',
-    'Lec3': 'https://drive.google.com/file/d/1kr1W-ttcehExOF1NTufeR-mQ3bdxfUwX/view?usp=drive_link',
-    'Lec4': 'https://drive.google.com/file/d/1zHqAPIpThou6DP665PAohYgn8PkBKggx/view?usp=drive_link',
-    'Lec5': 'https://drive.google.com/file/d/11exolkKzm0RxYu1mK1L6W7Tw9-yY7hqL/view?usp=drive_link',
-    'Lec6': 'https://drive.google.com/file/d/1untXdV9kN5K8Du2ntddbi1KVmPwRJNli/view?usp=drive_link'
+    'Lec1': 'Ur link',
+    'Lec2': 'Ur link',
+    'Lec3': 'Ur link',
+    'Lec4': 'Ur link',
+    'Lec5': 'Ur link',
+    'Lec6': 'Ur link'
   },
   'descrete': {
-    'Lec1': 'https://drive.google.com/file/d/1joHEEvtlSzzv_Hl_ILkksjVVtDFkneoQ/view?usp=drive_link'
+    'Lec1': 'Ur link'
   },
   'computer': {
-    'Lec1': 'https://drive.google.com/file/d/1kbtKPU_-G87Q1FMbkhgtq8jHbpWwy9h5/view?usp=drive_link'
+    'Lec1': 'Ur link'
   }
 };
 
